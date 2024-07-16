a5cc7589ac5d7874cbcb5029a474d5855b35cb29
Gamletel <gamletel@list.ru>
2024-07-16 17:29:33 +0300

    Added NextJS

---
+++ nextJS/src/app/users/page.js
@@ -0,0 +1,18 @@
+'use client'
+
+import {useEffect, useState} from "react";
+
+export default function Page(){
+    const [data, setData] = useState(null)
+
+    useEffect(()=>{
+        fetch('http://127.0.0.1:8000/api/users')
+            .then((res)=>res.json())
+            .then((data)=>{
+                setData(data)
+            })
+    }, [])
+    console.log(data);
+
+
+}
