a159fe0a07b060555f2248c409a3b495c648012e
Gamletel <gamletel@list.ru>
2024-07-17 07:14:50 +0300

    Added api to routing

--- laravel-backend/routes/api.php
+++ laravel-backend/routes/api.php
@@ -1,4 +1,4 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('api/users', [\App\Http\Controllers\UserController::class, 'index'])->name('user.index');
+Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('user.index');
